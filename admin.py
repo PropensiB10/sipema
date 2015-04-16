af5632bbfe443b5194ba693fd0a5f73c8e194b8b
asriwkei <asriwahyukei@gmail.com>
2015-04-16 19:05:31 +0700

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,54 +1,61 @@
 from django.contrib import admin
 
 # Register your models here.
-from polls.models import User
-from polls.models import Jadwal_kelas
-from polls.models import Food
-from polls.models import Order
-from polls.models import Order_item
-from polls.models import Review
-from polls.models import Pembayaran
+from .models import *
 
 class UserAdmin(admin.ModelAdmin):
 	list_display = ['username','nama_user','role']
+	search_fields = ['username','nama_user','role']
 	class Meta:
 		model = User
 
-class Jadwal_kelasAdmin(admin.ModelAdmin):
-	list_display = ['dosen','hari','jammulai','jamselesai','ruangan']
+class JadwalKelasAdmin(admin.ModelAdmin):
+	list_display = ['dosen','hari','jam_mulai','jam_selesai','ruangan']
+	search_fields = ['dosen','hari','jam_mulai','jam_selesai','ruangan']
 	class Meta:
-		model = Jadwal_kelas
+		model = JadwalKelas
 
+class RestaurantAdmin(admin.ModelAdmin):
+	list_display = ['nama_restoran']
+	search_fields =['nama_restoran']
+	class Meta:
+		model = Restaurant
+		
 class FoodAdmin(admin.ModelAdmin):
-	list_display = ['nama','total_rating']
+	list_display = ['restoran','nama_makanan','total_rating']
+	search_fields = ['restoran','nama_makanan','total_rating'] 
 	class Meta:
 		model = Food
 
 class OrderAdmin(admin.ModelAdmin):
 	list_display = ['waktu_order','dosen']
+	search_fields = ['waktu_order','dosen']
 	class Meta:
 		model = Order
 
-class Order_itemAdmin(admin.ModelAdmin):
-	list_display = ['order','food','qty','consumer_type']
+class OrderItemAdmin(admin.ModelAdmin):
+	list_display = ['order','food','kuantitas','tipe_konsumen','permintaan_lain']
+	search_fields = ['order','food','kuantitas','tipe_konsumen','permintaan_lain']
 	class Meta:
-		model = Order_item
+		model = OrderItem
 
 class ReviewAdmin(admin.ModelAdmin):
 	list_display = ['food','rating','komentar','dosen']
+	search_fields = ['food','rating','komentar','dosen']
 	class Meta:
 		model = Review
 
 class PembayaranAdmin(admin.ModelAdmin):
 	list_display = ['waktu_bayar','sekretariat']
+	search_fields = ['waktu_bayar','sekretariat']
 	class Meta:
-		model = Pembayaran
+		model = Pembayaran		
 		
 admin.site.register(User, UserAdmin)
-admin.site.register(Jadwal_kelas, Jadwal_kelasAdmin)
+admin.site.register(JadwalKelas, JadwalKelasAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
 admin.site.register(Food, FoodAdmin)
 admin.site.register(Order, OrderAdmin)
-admin.site.register(Order_item, Order_itemAdmin)
+admin.site.register(OrderItem, OrderItemAdmin)
 admin.site.register(Review, ReviewAdmin)
 admin.site.register(Pembayaran, PembayaranAdmin)
-
